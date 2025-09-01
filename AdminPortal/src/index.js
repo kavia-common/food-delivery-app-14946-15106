ee42d09339e1e73eb0036ff8832460dfee616f97
Kavia AI <production@kavia.ai>
2025-09-01 07:05:16 +0000

    CheckPoint - cg49bfab96

--- AdminPortal/src/index.js
+++ AdminPortal/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
